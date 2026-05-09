3c1cf6cd6de130af2ab66fbee676909b499b693e
Cheryl-Chun <769303522@qq.com>
2026-05-09 10:17:39 +0800

    add auth middleware

---
+++ src/middleware/auth.rs
@@ -0,0 +1,38 @@
+use crate::{error::ProxyError, server::AppState};
+use axum::{
+    extract::{Request, State},
+    middleware::Next,
+    response::Response,
+};
+use reqwest::header::AUTHORIZATION;
+
+pub async fn auth_middleware(
+    State(state): State<AppState>,
+    req: Request,
+    next: Next,
+) -> Result<Response, ProxyError> {
+    let auth_header = req
+        .headers()
+        .get(AUTHORIZATION)
+        .and_then(|h| h.to_str().ok())
+        .ok_or_else(|| ProxyError::Auth("Missing Authorization header".to_string()))?;
+
+    if !auth_header.starts_with("Bearer ") {
+        return Err(ProxyError::Auth("Invalid token format".to_string()));
+    }
+
+    if valid_token(auth_header) {
+        let response = next.run(req).await;
+        Ok(response)
+    } else {
+        Err(ProxyError::Auth(format!(
+            "Invalid or expired API key: {}",
+            auth_header
+        )))
+    }
+}
+
+// TODO: Token 校验
+fn valid_token(token: &str) -> bool {
+    return false;
+}
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -9,10 +9,14 @@ use tower_http::trace::TraceLayer;
 
 pub use state::AppState;
 
+use crate::middleware;
+
+
 pub fn create_router(app_state: AppState) -> Router {
     Router::new()
         .route("/:provider/chat/completions", post(handler::chat_handler))
         .route("/health", get(handler::health_handler))
+        .layer(axum::middleware::from_fn_with_state(app_state.clone(), middleware::auth_middleware))
         .layer(TraceLayer::new_for_http())
         .with_state(app_state)
 }
